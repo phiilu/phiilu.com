d641d4bc1cef820cecf84fb1567c718237e2f062
Florian Kapfenberger <florian@kapfenberger.me>
2020-10-24 22:42:08 +0200

    chore: update build command to generate static build

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
-  distDir: 'build',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,

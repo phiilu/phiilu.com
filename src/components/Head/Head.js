4f31056a7a8b25a5e0e0713741aab9b3666b3273
Florian Kapfenberger <florian@kapfenberger.me>
2020-10-28 19:38:49 +0100

    feat: add favicon to page

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -31,6 +31,14 @@ const Head = ({ children, title, description, url, date, image }) => {
 
       {url && <link rel="canonical" href={url} />}
 
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#667eea" />
+      <meta name="msapplication-TileColor" content="#667eea" />
+      <meta name="theme-color" content="#667eea" />
+
       <title>{title} | Phiilu</title>
 
       {children}

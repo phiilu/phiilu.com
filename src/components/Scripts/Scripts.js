b1ce75d105acafdcae8b5e42affe3545a24de379
Florian Kapfenberger <florian@kapfenberger.me>
2020-10-29 20:49:02 +0100

    feat: add fathom and simple analytics to page to test them for my blog post

--- src/components/Scripts/Scripts.js
+++ src/components/Scripts/Scripts.js
@@ -5,6 +5,7 @@ function Scripts() {
 
   return (
     <>
+      {/* Umami Analytics */}
       <script
         async
         defer
@@ -12,12 +13,20 @@ function Scripts() {
         data-domains="phiilu.com"
         data-website-id={process.env.UMAMI_WEBSITE_ID}
         src="https://u.phiilu.com/umami.js"></script>
+      {/* Plausible Analytics */}
       <script
         async
         defer
         data-domain="phiilu.com"
         src="https://p.phiilu.com/js/plausible.js"></script>
+      {/* Splitbee */}
       <script async defer src="https://cdn.splitbee.io/sb.js"></script>
+      {/* Simple Analytics */}
+      <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+      <noscript>
+        <img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" />
+      </noscript>
+      {/* Fathom is in _app.js */}
     </>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,32 @@
 import '../styles/index.css';
 import GoogleFonts from 'next-google-fonts';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as FathomClient from 'fathom-client';
+
+function Fathom() {
+  const router = useRouter();
+
+  useEffect(() => {
+    FathomClient.load(process.env.FATHOM_SITE_ID, {
+      includedDomains: ['phiilu.com']
+    });
+
+    function onRouteChangeComplete() {
+      FathomClient.trackPageview();
+    }
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+}
 
 function PhiiluBlog({ Component, pageProps }) {
   return (
     <>
+      <Fathom />
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" />
       <Component {...pageProps} />
     </>

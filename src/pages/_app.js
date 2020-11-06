48e4a93722b17751adee8439c386c1c51e5f7b75
Florian Kapfenberger <florian@kapfenberger.me>
2020-11-06 19:48:50 +0100

    fix: Fathom test finished

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,9 @@
 import '../styles/index.css';
 import GoogleFonts from 'next-google-fonts';
-import Fathom from '@components/Fathom/Fathom';
 
 function PhiiluBlog({ Component, pageProps }) {
   return (
     <>
-      <Fathom />
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" />
       <Component {...pageProps} />
     </>

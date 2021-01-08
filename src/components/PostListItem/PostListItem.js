6260e5e384e1cc5f2b9ac8069ae42d78a8cc3a5c
Florian Kapfenberger <florian@kapfenberger.me>
2021-01-08 16:28:18 +0100

    fix: PostListItem icons on mobile

--- src/components/PostListItem/PostListItem.js
+++ src/components/PostListItem/PostListItem.js
@@ -41,7 +41,7 @@ const PostListItem = ({ post: { slug, title, description, published, icon, date,
                 height={48}
                 width={48}
                 alt={icon.alt}
-                className="object-cover"
+                className="object-contain"
               />
             </div>
             <div>

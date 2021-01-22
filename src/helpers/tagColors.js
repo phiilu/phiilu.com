4fd40435a1c037e0e31dfc3aeb92fa734249c28e
Florian Kapfenberger <florian@kapfenberger.me>
2021-01-22 20:50:30 +0100

    feat: update tag colors for dark mode

--- src/helpers/tagColors.js
+++ src/helpers/tagColors.js
@@ -55,65 +55,65 @@ const tags = {
     bg: 'bg-purple-200 dark:bg-purple-500',
     text: 'text-purple-800 dark:text-white',
     mainColor: 'text-purple-500',
-    hover: 'hover:bg-purple-500 hover:text-white',
+    hover: 'hover:bg-purple-500 hover:text-white dark:hover:bg-purple-400',
     description: 'This tag shows posts that are taking advantage of Virtual Private Servers.'
   },
   vpn: {
     name: 'vpn',
     slug: slugify('vpn'),
-    bg: 'bg-indigo-200 ',
-    text: 'text-indigo-800',
+    bg: 'bg-indigo-200 dark:bg-indigo-500 ',
+    text: 'text-indigo-800 dark:text-white',
     mainColor: 'text-vpn-500',
-    hover: 'hover:bg-indigo-500 hover:text-white',
+    hover: 'hover:bg-indigo-500 hover:text-white dark:hover:bg-indigo-400',
     description:
       'A virtual private network (VPN) extends a private network across a public network and enables users to connect to computers as if they were in the same network. Today VPNs are also used to surf the internet more secure and anonymous using various VPN Providers.'
   },
   vultr: {
     name: 'vultr',
     slug: slugify('vultr'),
-    bg: 'bg-vultr-200 ',
-    text: 'text-vultr-800',
+    bg: 'bg-vultr-200 dark:bg-vultr-500',
+    text: 'text-vultr-800 dark:text-white',
     mainColor: 'text-vultr-500',
-    hover: 'hover:bg-vultr-500 hover:text-white',
+    hover: 'hover:bg-vultr-500 hover:text-white dark:hover:bg-vultr-400',
     description:
       'This tag shows posts using Vultr VPS. Vultr is a great VPS provider with performant servers backed by benchmarks.'
   },
   ubuntu: {
     name: 'ubuntu',
     slug: slugify('ubuntu'),
-    bg: 'bg-ubuntu-200',
-    text: 'text-ubuntu-800',
+    bg: 'bg-ubuntu-200 dark:bg-ubuntu-500',
+    text: 'text-ubuntu-800 dark:text-white',
     mainColor: 'text-ubuntu-500',
-    hover: 'hover:bg-ubuntu-500 hover:text-white',
+    hover: 'hover:bg-ubuntu-500 hover:text-white dark:hover:bg-ubuntu-400',
     description:
       'This tag shows posts that contain information about Ubuntu configuration. Ubuntu is a Linux distribution based on Debian.'
   },
   'hello world': {
     name: 'hello world',
     slug: slugify('hello world'),
-    bg: 'bg-red-200',
-    text: 'text-red-800',
+    bg: 'bg-red-200 dark:bg-red-500',
+    text: 'text-red-800 dark:text-white',
     mainColor: 'text-red-500',
-    hover: 'hover:bg-red-500 hover:text-white',
+    hover: 'hover:bg-red-500 hover:text-white dark:hover:bg-red-400',
     description: 'Hello World. This is my first tag ever!'
   },
   2020: {
     name: '2020',
     slug: slugify('2020'),
-    bg: 'bg-pink-200',
-    text: 'text-pink-800',
+    bg: 'bg-pink-200 dark:bg-pink-500',
+    text: 'text-pink-800 dark:text-white',
     mainColor: 'text-pink-500',
-    hover: 'hover:bg-pink-500 hover:text-white',
+    hover: 'hover:bg-pink-500 hover:text-white dark:hover:bg-pink-400',
     description:
       'What a great year right? COVID-19 hit the world pretty hard and lots of other things happened, too many to list them all in this short description.'
   },
   server: {
     name: 'server',
     slug: slugify('server'),
-    bg: 'bg-teal-200',
-    text: 'text-teal-800',
+    bg: 'bg-teal-200 dark:bg-teal-500',
+    text: 'text-teal-800 dark:text-white',
     mainColor: 'text-teal-500',
-    hover: 'hover:bg-teal-500 hover:text-white',
+    hover: 'hover:bg-teal-500 hover:text-white dark:hover:bg-teal-400',
     description:
       'Posts that explain how to install, configure or interact with servers will show up here.'
   },
@@ -129,7 +129,7 @@ const tags = {
   terminal: {
     name: 'terminal',
     slug: slugify('terminal'),
-    bg: 'bg-gray-900',
+    bg: 'bg-gray-900 dark:bg-black',
     text: 'text-white',
     mainColor: 'text-gray-900',
     hover: 'hover:bg-gray-900 hover:text-green-400',
@@ -138,54 +138,54 @@ const tags = {
   network: {
     name: 'network',
     slug: slugify('network'),
-    bg: 'bg-gray-200',
-    text: 'text-gray-800',
+    bg: 'bg-gray-200 dark:bg-gray-500',
+    text: 'text-gray-800 dark:text-white',
     mainColor: 'text-gray-500',
-    hover: 'hover:bg-gray-500 hover:text-white',
+    hover: 'hover:bg-gray-500 hover:text-white dark:hover:bg-gray-400',
     description: 'Posts that are heavy Javascript focused will be listed here.'
   },
   serverless: {
     name: 'serverless',
     slug: slugify('serverless'),
-    bg: 'bg-serverless-200',
-    text: 'text-serverless-800',
+    bg: 'bg-serverless-200 dark:bg-serverless-500',
+    text: 'text-serverless-800 dark:text-white',
     mainColor: 'text-serverless-500',
-    hover: 'hover:bg-serverless-500 hover:text-white',
+    hover: 'hover:bg-serverless-500 hover:text-white dark:hover:bg-serverless-400',
     description:
       'Serverless or Lambda functions is a new way of programming. It removes the need of hosting and maintaining your own server, but instead you publish your code (functions) on services and pay per invocation.'
   },
   netlify: {
     name: 'netlify',
     slug: slugify('netlify'),
-    bg: 'bg-netlify-100',
-    text: 'text-netlify-600',
+    bg: 'bg-netlify-100 dark:bg-netlify-500',
+    text: 'text-netlify-600 dark:text-white',
     mainColor: 'text-netlify-500',
-    hover: 'hover:bg-netlify-500 hover:text-white',
+    hover: 'hover:bg-netlify-500 hover:text-white dark:hover:bg-netlify-400 dark:hover:text-black',
     description: 'Netlify is a cloud service provider where you can host your static websites!'
   },
   'apple maps': {
     name: 'apple maps',
     slug: slugify('apple maps'),
-    bg: 'bg-gray-100',
-    text: 'text-gray-900',
+    bg: 'bg-gray-100 dark:bg-gray-500',
+    text: 'text-gray-900 dark:text-white',
     mainColor: 'text-gray-900',
-    hover: 'hover:bg-gray-500 hover:text-white',
+    hover: 'hover:bg-gray-500 hover:text-white dark:hover:bg-gray-400',
     description: 'Apple Maps is a map software developed by Apple'
   },
   html: {
     name: 'html',
     slug: slugify('html'),
-    bg: 'bg-html-200',
-    text: 'text-html-800',
+    bg: 'bg-html-200 dark:bg-html-500',
+    text: 'text-html-800 dark:text-white',
     mainColor: 'text-html-500',
-    hover: 'hover:bg-html-500 hover:text-white',
+    hover: 'hover:bg-html-500 hover:text-white dark:hover:bg-html-400',
     description:
       'Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser.'
   },
   react: {
     name: 'react',
     slug: slugify('react'),
-    bg: 'bg-react-gray-500',
+    bg: 'bg-react-gray-500 dark:bg-react-gray-900',
     text: 'text-react-500',
     mainColor: 'text-react-500',
     hover: 'hover:bg-react-500 hover:text-react-gray-500',
@@ -203,36 +203,36 @@ const tags = {
   analytics: {
     name: 'analytics',
     slug: slugify('analytics'),
-    bg: 'bg-green-200',
-    text: 'text-green-900',
-    hover: 'hover:bg-green-500 hover:text-white',
+    bg: 'bg-green-200 dark:bg-green-500',
+    text: 'text-green-900 dark:text-white',
+    hover: 'hover:bg-green-500 hover:text-white dark:hover:bg-green-400',
     mainColor: 'text-green-500',
     description: 'Analytics'
   },
   privacy: {
     name: 'privacy',
     slug: slugify('privacy'),
-    bg: 'bg-blue-200',
-    text: 'text-blue-900',
-    hover: 'hover:bg-blue-500 hover:text-white',
+    bg: 'bg-blue-200 dark:bg-blue-500',
+    text: 'text-blue-900 dark:text-white',
+    hover: 'hover:bg-blue-500 hover:text-white dark:hover:bg-blue-400',
     mainColor: 'text-blue-500',
     description: 'Privacy'
   },
   seo: {
     name: 'seo',
     slug: slugify('seo'),
-    bg: 'bg-green-200',
-    text: 'text-green-900',
-    hover: 'hover:bg-green-500 hover:text-white',
+    bg: 'bg-green-200 dark:bg-green-500',
+    text: 'text-green-900 dark:text-white',
+    hover: 'hover:bg-green-500 hover:text-white dark:hover:bg-green-400',
     mainColor: 'text-green-500',
     description: 'SEO'
   },
   docker: {
     name: 'docker',
     slug: slugify('docker'),
-    bg: 'bg-blue-200',
-    text: 'text-blue-900',
-    hover: 'hover:bg-blue-500 hover:text-white',
+    bg: 'bg-blue-200 dark:bg-blue-500',
+    text: 'text-blue-900 dark:text-white',
+    hover: 'hover:bg-blue-500 hover:text-white dark:hover:bg-blue-400',
     mainColor: 'text-blue-500',
     description: 'Docker'
   }

961a6b600279aab4760f431156d110994a323e51
Florian Kapfenberger <florian@kapfenberger.me>
2023-07-17 18:56:56 +0200

    fix: birthday count up

--- src/components/react/Age.tsx
+++ src/components/react/Age.tsx
@@ -8,7 +8,7 @@ export function Age({ age: ageFromProps }: AgeProps) {
   const [age, setAge] = useState(() => ageFromProps);
   useEffect(() => {
     const interval = setInterval(() => {
-      setAge((age) => age + 1000);
+      setAge((age) => age + 1);
     }, 1000);
 
     return () => clearInterval(interval);

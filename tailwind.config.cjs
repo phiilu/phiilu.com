606fb0c9b26d32514ad520bcec4524c626bd8184
Florian Kapfenberger <florian@kapfenberger.me>
2023-07-17 19:36:01 +0200

    fix: tailwind font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "open-sans": "Open Sans, sans-serif",
-        "source-sans-pro": "Source Sans Pro, sans-serif",
+        "open-sans": "'Open Sans', sans-serif",
+        "source-sans-pro": "'Source Sans Pro', sans-serif",
       },
       lineHeight: {
         14: "3rem",

d8c7e5b82d85abffe225af77cf8e7496832f30e9
Florian Kapfenberger <florian@kapfenberger.me>
2023-07-18 00:19:25 +0200

    fix: add default title

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
+export const SITE_TITLE = 'Phiilu | Florian Kapfenberger';
 export const SITE_DESCRIPTION =
   'Software Developer: TypeScript, Elixir & Swift / Indie Hacker / Loves to self-host apps and services';

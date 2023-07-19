bb4940eddf34f73a7108075da3f2b6eb05539ecb
Florian Kapfenberger <florian@kapfenberger.me>
2023-07-19 23:56:56 +0200

    fix: SEO issues

--- src/components/react/SectionHeading.tsx
+++ src/components/react/SectionHeading.tsx
@@ -12,7 +12,9 @@ export function SectionHeading({ heading, subheading, children }: SectionHeading
       <p className="font-semibold leading-6 tracking-wide text-indigo-600 uppercase text-md xl:text-xl xl:leading-6 dark:text-indigo-500">
         {heading}
       </p>
-      <Heading noMargin>{subheading}</Heading>
+      <Heading as="h2" noMargin>
+        {subheading}
+      </Heading>
       <p className="max-w-2xl mt-4 text-xl leading-9 text-gray-500 xl:text-xl dark:text-gray-100 lg:mx-auto xl:leading-9">
         {children}
       </p>
